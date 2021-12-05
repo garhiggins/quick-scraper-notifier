85e8a59705570f3a63bbd3d63c7d73577233e879
Gar Higgins <gar@cambly.com>
2021-12-05 14:36:36 -0800

    use environment vars

--- qsn/qsn.py
+++ qsn/qsn.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import requests
 from twilio.rest import Client
 
@@ -13,22 +14,27 @@ parser.add_argument("--twilio_phone_from")
 
 args = parser.parse_args()
 
-client = Client(args.twilio_sid, args.twilio_token)
+twilio_sid = args.twilio_sid or os.environ.get("TWILIO_SID")
+twilio_token = args.twilio_token or os.environ.get("TWILIO_TOKEN")
+twilio_phone_to = args.twilio_phone_to or os.environ.get("TWILIO_PHONE_TO")
+twilio_phone_from = args.twilio_phone_from or os.environ.get("TWILIO_PHONE_FROM")
+
+client = Client(twilio_sid, twilio_token)
 
 response = requests.get(args.url).text
 if args.notify_if_present:
     print(f"present is {args.notify_if_present}")
     if args.notify_if_present in response:
         client.messages.create(
-            to=args.twilio_phone_to,
-            from_=args.twilio_phone_from,
+            to=twilio_phone_to,
+            from_=twilio_phone_from,
             body=f"QSN: {args.notify_if_present} found at {args.url}"
         )
 if args.notify_if_missing:
     print(f"missing is {args.notify_if_missing}")
     if args.notify_if_missing not in response:
         client.messages.create(
-            to=args.twilio_phone_to,
-            from_=args.twilio_phone_from,
+            to=twilio_phone_to,
+            from_=twilio_phone_from,
             body=f"QSN: {args.notify_if_missing} missing from {args.url}"
         )
